d65ea68093d5097c07be11524d0926157243232e
tranduchiep <tranhiep92@gmail.com>
2015-08-24 15:27:35 +0700

    met

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
-User.create!(id: 1, email: "tranhiep92@gmail.com", password: "1tranduchiep", password_confirmation: "1tranduchiep", admin: true, status: "Active")
-User.create!(id: 2, email: "admin@gmail.com", password: "1tranduchiep", password_confirmation: "1tranduchiep", admin: true, status: "Active")
+User.create!(email: 'tranhiep92@gmail.com', 
+             password: 'tranduchiep', 
+             password_confirmation: 'tranduchiep',
+             admin: true, status: "Active")
