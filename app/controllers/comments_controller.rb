17119ca74ba900da43c3917e8bbd23d50ef5d694
tranduchiep <tranhiep92@gmail.com>
2015-09-12 11:49:42 +0700

    update comment action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
+
    def create
-   
      comment = Comment.create!(user_id: current_user.id, product_id: params[:product_id],
                        title: params[:title], message: params[:message], rate: params[:rate]).valid?
      rating = Comment.where(product_id: params[:product_id]).average(:rate).to_f
@@ -13,4 +13,23 @@ class CommentsController < ApplicationController
        redirect_to :back
      end 
    end
+   
+   
+   def update
+     if Comment.find(params[:id]).update!(title: params[:title], message: params[:message], rate: params[:rate],
+                                          user_id: current_user.id, product_id: params[:product_id])
+        flash[:success] = "Update comment complete."
+        redirect_to :back
+     else
+        flash[:error] = "Update comment fail !!!"
+        redirect_to :back
+     end
+   end
+   
+   def destroy
+     Comment.find(params[:id]).destroy
+     flash[:success] = "Remove comment complete."
+     redirect_to :back
+   end
+   
 end
