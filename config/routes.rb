11ade8b0b2be1c46970219b55da73edad5456f83
tranduchiep <tranhiep92@gmail.com>
2015-09-17 17:42:31 +0700

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,9 @@ Rails.application.routes.draw do
     get '/active-complete', to: 'home#active_complete'
     post 'active', to: 'home#active'
   end
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 
   namespace :admin do
     root to: "users#index"
